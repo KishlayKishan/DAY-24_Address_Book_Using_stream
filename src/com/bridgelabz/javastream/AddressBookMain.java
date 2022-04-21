c2edf5d59d0c66509a1c0d190c0fd4b8173eae70
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-21 22:12:44 +0530

    Added:Add Multiple contacts in address book

--- src/com/bridgelabz/javastream/AddressBookMain.java
+++ src/com/bridgelabz/javastream/AddressBookMain.java
@@ -1,24 +1,38 @@
 package com.bridgelabz.javastream;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+	static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		System.out.println("....WELCOME TO ADDRESS BOOK....");
-		Contacts newContact = new Contacts();
-		newContact.setFirstName("Kishlay \n");
-		newContact.setLastName("Kishan \n");
-		newContact.setAddress("Area-Aghoria Bazar, Dist-Muzaffarpur, State-Bihar \n");
-		newContact.setCity("Muzaffarpur \n");
-		newContact.setState("Bihar \n");
-		newContact.setZip(842002);
-		newContact.setEmail("kishan.kishlay20@gmail.com \n");
-		newContact.setPhoneNumber(1234567890);
-		System.out.println("The Contact is : \n" + newContact);
-
-		// To add new contact
-
+		System.out.println("\n Welcome to Address Book System");
 		AddressBook addressBook = new AddressBook();
-		addressBook.operation();
+		boolean quit = false;
+		while (!quit) {
+			System.out.println("Enter \n1 -> Add Contact \n2 -> Edit Existing Contact \n3 -> Delete contact "
+					+ "\n4 -> Add multiple contact \n5 -> Quit ");
+			switch (scanner.nextInt()) {
+			case 1:
+				addressBook.addContacts();
+				break;
+			case 2:
+				addressBook.edit();
+				break;
+			case 3:
+				addressBook.delete();
+				break;
+			case 4:
+				addressBook.addMultipleContacts();
+				break;
+			case 5:
+				System.out.println("Exiting fro AddressBook");
+				quit = true;
+				break;
+			default:
+				System.out.println("Enter valid Number");
 
+			}
+		}
 	}
 }
