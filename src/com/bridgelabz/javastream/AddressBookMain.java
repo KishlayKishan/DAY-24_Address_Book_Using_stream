e77f57f530826b15af6f2ebac6f8f92896d9b0a0
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-21 21:24:47 +0530

    Added:Create own contact in address book

---
+++ src/com/bridgelabz/javastream/AddressBookMain.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.javastream;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("....WELCOME TO ADDRESS BOOK....");
+		Contacts newContact = new Contacts();
+		newContact.setFirstName("Kishlay \n");
+		newContact.setLastName("Kishan \n");
+		newContact.setAddress("Area-Aghoria Bazar, Dist-Muzaffarpur, State-Bihar \n");
+		newContact.setCity("Muzaffarpur \n");
+		newContact.setState("Bihar \n");
+		newContact.setZip(842002);
+		newContact.setEmail("kishan.kishlay20@gmail.com \n");
+		newContact.setPhoneNumber(1234567890);
+		System.out.println("The Contact is : \n" + newContact);
+
+	}
+}
